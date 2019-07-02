58f503287546e66941b2d6e97d900a6c0faf622b
Amanda Chau <amandachau36@gmail.com>
2019-07-02 17:34:40 +1000

    created LogIn component

---
+++ src/LogIn.jsx
@@ -0,0 +1,15 @@
+import React, {Component } from 'React';
+import { Link } from 'react-router-dom';
+
+class LogIn extends Component {
+  render(){
+    return(
+      <div>
+        <h1>LogIn page</h1>
+      </div>
+    )
+  }
+
+}
+
+export default LogIn;
