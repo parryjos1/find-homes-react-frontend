3bb7f725cf87e9f1bafb122986fe020eeafca2fa
Amanda Chau <amandachau36@gmail.com>
2019-07-04 10:01:16 +1000

    Fixed wishlist loading problem

--- src/WishList.jsx
+++ src/WishList.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 
-const userToken = localStorage.getItem('userToken');
-console.log('localStorage in WishList Component', userToken);
-const authStr =  "Bearer " + userToken;
-console.log(authStr);
 
 class WishList extends Component {
   constructor(){
@@ -24,7 +20,11 @@ class WishList extends Component {
   getWishList = () => {
     //userToken is stored in localStorage
 
-    axios.get('http://localhost:3000/wishlists', { headers: {Authorization: authStr }})
+    axios.get('http://localhost:3000/wishlists', {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem('userToken')
+      }
+    })
     .then( res =>{
       console.log(res.data.listings);
       console.log(res.data.name);
@@ -37,19 +37,33 @@ class WishList extends Component {
     })
     .catch( err => {
       console.log(err);
-    })
+    });
   }
 
-  deleteListing = (id) => {
+  deleteListing = (id, index) => {
     console.log('delete listing id', id);
+    console.log('index', index)
 
-    axios.delete(`http://localhost:3000/wishlists/${id}`, { headers: {Authorization: authStr }})
+    axios.delete(`http://localhost:3000/wishlists/` + id, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem('userToken')
+      }
+    })
     // { data: { listingId: id }}
     .then( res =>{
       console.log(res);
+      // update this.state.listings, removing the item that was deleted
+      // so the rendered view no longer shows it
+      const newListings = [...this.state.listings];
+      // console.log(newListings);
+      newListings.splice(index, 1);
+      // console.log(newListings);
+      this.setState({
+        listings: newListings
+      })
     }).catch( err => {
         console.log(err);
-    })
+    });
   }
 
 
@@ -61,14 +75,14 @@ class WishList extends Component {
         {
           this.state.listings.length > 0
           ?
-          this.state.listings.map( listing => (
+          this.state.listings.map( (listing, index) => (
               <div key={listing.id} className="listings">
                 <div className="listings-left">
                   <p><strong>{listing.headline}</strong></p>
                   <p>{listing.address}</p>
                 </div><div className="listings-right">
                   <img src={listing.image}/>
-                  <button onClick={ ()=>this.deleteListing(listing.id) }> Delete
+                  <button onClick={ ()=>this.deleteListing(listing.id, index) }> Delete
                   </button>
                 </div>
               </div>
