17614890238db961951d29cb87876388947166b8
Amanda Chau <amandachau36@gmail.com>
2019-07-05 11:42:51 +1000

    Error messages for create account

--- src/CreateAccount.jsx
+++ src/CreateAccount.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+let jwt = '';
+
 class CreateAccount extends Component {
   constructor(){
     super();
@@ -9,7 +11,7 @@ class CreateAccount extends Component {
       name: '',
       email: '',
       password: '',
-      errorMessage: ''
+      errorMessage: [],
     }
   } // end of constructor
 
@@ -40,23 +42,38 @@ class CreateAccount extends Component {
     // console.log(this.state.password);
     this.createUser();
 
-
-
-
-
   } // end of handleSubmit
 
   createUser = () => {
-    axios.post('http://localhost:3000/user', { name: this.state.name, email: this.state.email, password: this.state.password
+    axios.post('http://localhost:3000/user', { user: {name: this.state.name, email: this.state.email, password: this.state.password}
     }).then( res => {
-      console.log(res)
+      console.log(res);
+
+      this.getToken();
+
+
     }).catch( err => {
-      console.log(err);
+      console.log(err.response.data.errors);
+      this.setState({
+        errorMessage: err.response.data.errors
+      })
     })
-
-
   }
 
+  getToken = () => {
+    axios.post('http://localhost:3000/user_token', {"auth": {"email": this.state.email, "password": this.state.password}})
+    .then( res => {
+
+    jwt = res.data.jwt;
+    console.log(jwt)
+
+    localStorage.setItem('userToken', jwt);
+    this.props.history.push('/wishlist');
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  }
 
 
 
@@ -64,6 +81,17 @@ class CreateAccount extends Component {
     return(
       <div>
         <h1>Create a new account</h1>
+        <div style={{color: 'red'}}>
+          <ul>
+          {
+              this.state.errorMessage.map( error => (
+              <li>{error}</li>
+              ))
+          }
+
+          </ul>
+
+        </div>
         <form onSubmit={ this.handleSubmit }>
           <input type="text" placeholder="Your name" onChange={ this.handleInputName }></input><br/>
           <input type="text" placeholder="Email" onChange={ this.handleInputEmail }></input><br/>
