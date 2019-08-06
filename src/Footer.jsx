b28066227e1950aa327f7f878b257712ce87c80a
Amanda Chau <amandachau36@gmail.com>
2019-08-06 13:06:18 +1000

    Created Footer component and CSS

---
+++ src/Footer.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+// class Footer extends Component {
+//
+//   render(){
+//     return(
+//       <div>
+//         <h1>Hello footer</h1>
+//       </div>
+//     )
+//   }
+// }
+
+function Footer(){
+  return(
+    <div className="footer">
+      <div>&copy; 2019 FIND HOMES LTD</div><div>
+      <strong>CONTACT US</strong><br/>
+      <a target="_blank" href="https://amandachau36.github.io/portfolio/">Amanda Chau</a><br/>
+      <a target="_blank" href="http://www.josh-parry.com/">Josh Parry</a>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Footer;
